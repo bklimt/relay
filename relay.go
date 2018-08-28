92f726f445460f2db83447589963bb53dadfdd8f
Bryan Klimt <bklimt@gmail.com>
2018-08-28 08:21:26 -0700

    Start adding handlers for oauth.

--- relay.go
+++ relay.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"cloud.google.com/go/firestore"
@@ -14,7 +15,91 @@ import (
 	firebase "firebase.google.com/go"
 )
 
-func Serve(port uint16, app *firebase.App) {
+type Config struct {
+	ClientID string `json:"clientId"`
+}
+
+func Serve(port uint16, app *firebase.App, cfg *Config) {
+	// Redirects to the Nest login.
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Handling %s request to %s.\n", r.Method, r.RequestURI)
+
+		// Generate a state token.
+		client, err := app.Firestore(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "unable to initialize firestore: %s", err)
+			return
+		}
+		defer client.Close()
+
+		doc, _, err := client.Collection("auth").Add(r.Context(), map[string]interface{}{
+			"used": false,
+			"created": firestore.ServerTimestamp,
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "unable to write to firestore: %s", err)
+			return
+		}
+		state := doc.ID
+
+		// Redirect to the Nest oauth endpoint.
+		authUrl := fmt.Sprintf("https://home.nest.com/login/oauth2?client_id=%s&state=%s", cfg.ClientID, state)
+		w.Header().Add("Location", authUrl)
+		w.WriteHeader(http.StatusFound)
+		fmt.Fprintln(w, "Redirecting")
+	})
+
+	// Handles the oauth redirect from Nest.
+	http.HandleFunc("/oauth", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Handling %s request to /oauth.\n", r.Method)
+
+		params := r.URL.Query()
+		codes := params["code"]
+		states := params["state"]
+		if len(codes) == 0 || len(states) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "missing code or state")
+			return
+		}
+		//code := codes[0]
+		state := states[0]
+
+		// Check the state.
+		client, err := app.Firestore(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "unable to initialize firestore: %s", err)
+			return
+		}
+		defer client.Close()
+
+		doc, err := client.Collection("auth").Doc(state).Get(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "unable to read from firestore: %s", err)
+			return
+		}
+		data := doc.Data()
+		if data["used"] != false {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Fprint(w, "invalid oauth state")
+			return
+		}
+		data["used"] = true
+		data["updated"] = firestore.ServerTimestamp
+		_, err = client.Collection("auth").Doc(state).Set(r.Context(), data)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "unable to write to firestore: %s", err)
+			return
+		}
+
+		fmt.Fprintln(w, "Logged in successfully.")
+	})
+
+	// Logs a data snapshot to Firestore.
 	http.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Handling %s request to %s.\n", r.Method, r.RequestURI)
 
@@ -58,6 +143,26 @@ func Serve(port uint16, app *firebase.App) {
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
+func LoadConfig() *Config {
+	path := os.Getenv("KLIMT_RELAY_CONFIG")
+	if path == "" {
+		log.Fatal("KLIMT_RELAY_CONFIG environment variable must point to config")
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("error opening config at %q: %s", path, err)
+	}
+
+	var cfg *Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		log.Fatalf("error parsing config %q: %s", data, err)
+	}
+
+	return cfg
+}
+
 func InitFirebase() *firebase.App {
 	ctx := context.Background()
 	cfg := &firebase.Config{ProjectID: "home-d09a0"}
@@ -69,6 +174,7 @@ func InitFirebase() *firebase.App {
 }
 
 func main() {
+	cfg := LoadConfig()
 	app := InitFirebase()
-	Serve(8080, app)
+	Serve(8080, app, cfg)
 }
